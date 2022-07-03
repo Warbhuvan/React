2b9004703e2d1e6385fe03726770ac9c46acea4f
Warbhuvan <50885460+Warbhuvan@users.noreply.github.com>
2022-07-03 23:31:13 +0530

    Update Greet.js

--- src/Component/Greet.js
+++ src/Component/Greet.js
@@ -1,6 +1,12 @@
 import React from "react";
-function Greet()
+// function Greet()
+// {
+//     return <h1>Hello Sheetal</h1>
+// }
+
+//Name export
+export const Greet=()=>
 {
     return <h1>Hello Sheetal</h1>
 }
-export default Greet
+// export default Greet
