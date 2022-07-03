428b951f0964db23c7171f3da11885fa819b72e5
Warbhuvan <50885460+Warbhuvan@users.noreply.github.com>
2022-07-03 23:42:48 +0530

    used newly added class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-//import Greet from './Component/Greet';
 import {Greet} from './Component/Greet';
+import Welcome from './Component/Welcome';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Greet></Greet>
+        <Welcome/>
       </header>
     </div>
   );
