832c0f558f1994767fe50a896e0231b9b3b5c1c3
Warbhuvan <50885460+Warbhuvan@users.noreply.github.com>
2022-07-04 09:45:40 +0530

    Props demo

--- src/Component/Welcome.js
+++ src/Component/Welcome.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Component } from "react";
 
-class Welcome extends Component
-{
-    render()
-    {
-        return <h2>Welcome to react</h2>
-    }
+class Welcome extends Component {
+  render() {
+    return (
+      <h2>
+        Welcome {this.props.name} to {this.props.company}
+      </h2>
+    );
+  }
 }
-export default Welcome
+export default Welcome;
