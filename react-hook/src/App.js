c7afd8b0123a012fa3396df991498a046338f87b
Warbhuvan <50885460+Warbhuvan@users.noreply.github.com>
2022-07-09 20:53:13 +0530

    Update App.js
    Use State Hook demo

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import ClassCounter from './Component/ClassCounter';
+import StateHookCounter from './Component/StateHookCounter';
+import StateHookCounter2 from './Component/StateHookCounter2';
+import StateHookCounter3 from './Component/StateHookCounter3';
+import StateHookCounter4 from './Component/StateHookCounter4';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <ClassCounter></ClassCounter> */}
+        {/* <StateHookCounter></StateHookCounter> */}
+        {/* <StateHookCounter2></StateHookCounter2> */}
+        {/* <StateHookCounter3></StateHookCounter3> */}
+        <StateHookCounter4></StateHookCounter4>
       </header>
     </div>
   );
