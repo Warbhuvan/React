9d4eafc3fd068b7bf41e850cdb4381c4bea98319
Warbhuvan <50885460+Warbhuvan@users.noreply.github.com>
2022-07-09 17:18:21 +0530

    Update App.js
    Render props demo

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import ClickCounter from './Component/ClickCounter';
+import HoverCounter from './Component/HoverCounter';
+import User from './Component/User';
+import Counter from './Component/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <ClickCounter></ClickCounter>
+        <HoverCounter></HoverCounter>
+        <User render={(isLoggedIn)=> isLoggedIn?'Sheetal':'Guest'}></User> */}
+        <Counter render={(count,incrementCount)=>{
+          <ClickCounter count={count} incrementCount={incrementCount}></ClickCounter>
+        }}></Counter>
+        <Counter render={(count,incrementCount)=>{
+          <HoverCounter count={count} incrementCount={incrementCount}></HoverCounter>
+        }}></Counter>
       </header>
     </div>
   );
