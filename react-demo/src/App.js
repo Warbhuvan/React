cae6ee925b5012994cd7407d806bd5158d081b3c
Warbhuvan <50885460+Warbhuvan@users.noreply.github.com>
2022-07-09 17:43:32 +0530

    Update App.js
    Context demo

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import ClickCounter from './Component/ClickCounter';
-import HoverCounter from './Component/HoverCounter';
-import User from './Component/User';
-import Counter from './Component/Counter';
+import logo from "./logo.svg";
+import "./App.css";
+import ClickCounter from "./Component/ClickCounter";
+import HoverCounter from "./Component/HoverCounter";
+import User from "./Component/User";
+import Counter from "./Component/Counter";
+import ComponentC from "./Component/ComponentC";
+import { UserProvider } from "./Component/UserContext";
 
 function App() {
   return (
@@ -12,12 +14,26 @@ function App() {
         {/* <ClickCounter></ClickCounter>
         <HoverCounter></HoverCounter>
         <User render={(isLoggedIn)=> isLoggedIn?'Sheetal':'Guest'}></User> */}
-        <Counter render={(count,incrementCount)=>{
-          <ClickCounter count={count} incrementCount={incrementCount}></ClickCounter>
-        }}></Counter>
-        <Counter render={(count,incrementCount)=>{
-          <HoverCounter count={count} incrementCount={incrementCount}></HoverCounter>
-        }}></Counter>
+        <Counter
+          render={(count, incrementCount) => {
+            <ClickCounter
+              count={count}
+              incrementCount={incrementCount}
+            ></ClickCounter>;
+          }}
+        ></Counter>
+        <Counter
+          render={(count, incrementCount) => {
+            <HoverCounter
+              count={count}
+              incrementCount={incrementCount}
+            ></HoverCounter>;
+          }}
+        ></Counter>
+
+        <UserProvider value="Sheetal">
+          <ComponentC></ComponentC>
+        </UserProvider>
       </header>
     </div>
   );
