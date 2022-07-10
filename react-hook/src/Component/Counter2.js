0b2332e8777ee18046f317f0b1f9cdf8b3304a88
Warbhuvan <50885460+Warbhuvan@users.noreply.github.com>
2022-07-10 21:32:37 +0530

    Add files via upload
    UseReducer Hook

---
+++ react-hook/src/Component/Counter2.js
@@ -0,0 +1,49 @@
+import React, { useReducer } from "react";
+
+const initialState = { firstCounter: 0, secondCounter: 10 };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {...state, firstCounter: state.firstCounter + action.value };
+    case "decrement":
+      return { ...state,firstCounter: state.firstCounter - action.value };
+      case "incrementSecond":
+      return { ...state,secondCounter:state.secondCounter+action.value };
+    case "decrementSecond":
+      return {...state, secondCounter:state.secondCounter-action.value };
+    case "reset":
+      return { firstCounter: initialState.firstCounter,secondCounter:initialState.secondCounter };
+    default:
+      return {firstCounter: state.firstCounter,secondCounter:state.secondCounter  };
+  }
+};
+function Counter2() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      <div>First counter- {count.firstCounter}</div>
+      <div>Second Counter - {count.secondCounter}</div>
+      <button onClick={() => dispatch({ type: "increment", value: 1 })}>
+        Increment
+      </button>
+      <button onClick={() => dispatch({ type: "decrement", value: 1 })}>
+        Decrement
+      </button>
+      <button onClick={() => dispatch({ type: "incrementSecond", value: 1 })}>
+        Increment Second Counter
+      </button>
+      <button onClick={() => dispatch({ type: "decrementSecond", value: 1 })}>
+        Decrement Second Counter
+      </button>
+      <button onClick={() => dispatch({ type: "increment", value: 5 })}>
+        Increment 5
+      </button>
+      <button onClick={() => dispatch({ type: "decrement", value: 5 })}>
+        Decrement 5
+      </button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter2;
