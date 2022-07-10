efdd48e3fb56f95709fa0f54095618137571f569
Warbhuvan <50885460+Warbhuvan@users.noreply.github.com>
2022-07-10 14:00:51 +0530

    Update App.js
    Use Effect Hook

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -5,6 +5,11 @@ import StateHookCounter from './Component/StateHookCounter';
 import StateHookCounter2 from './Component/StateHookCounter2';
 import StateHookCounter3 from './Component/StateHookCounter3';
 import StateHookCounter4 from './Component/StateHookCounter4';
+import EffectCounter1 from './Component/EffectCounter1';
+import UseEffectHookMousePosition from './Component/UseEffectHookMousePosition';
+import MouseContainer from './Component/MouseContainer';
+import IntervalHookCounter from './Component/IntervalHookCounter';
+import DataFetching from './Component/DataFetching';
 
 function App() {
   return (
@@ -14,7 +19,11 @@ function App() {
         {/* <StateHookCounter></StateHookCounter> */}
         {/* <StateHookCounter2></StateHookCounter2> */}
         {/* <StateHookCounter3></StateHookCounter3> */}
-        <StateHookCounter4></StateHookCounter4>
+        {/* <StateHookCounter4></StateHookCounter4> */}
+        {/* <EffectCounter1></EffectCounter1> */}
+       {/* <MouseContainer></MouseContainer> */}
+       {/* <IntervalHookCounter></IntervalHookCounter> */}
+       <DataFetching></DataFetching>
       </header>
     </div>
   );
